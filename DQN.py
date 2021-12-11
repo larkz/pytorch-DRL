d978b8008216637ed20b95f14ef476ad33f424ed
larkz <liu.larkin@gmail.com>
2021-12-11 13:15:50 +0100

    debug

--- DQN.py
+++ DQN.py
@@ -61,10 +61,11 @@ class DQN(Agent):
         next_states_var = to_tensor_var(batch.next_states, self.use_cuda).view(-1, self.state_dim)
         dones_var = to_tensor_var(batch.dones, self.use_cuda).view(-1, 1)
 
-        print("DQN States/actions/rewards var")
+        print("DQN States/actions/rewards/dones var")
         print(states_var)
         print(actions_var)
         print(rewards_var)
+        print(dones_var)
 
         # compute Q(s_t, a) - the model computes Q(s_t), then we select the
         # columns of actions taken
