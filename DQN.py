8f033a91f933831e18a23ed35086c97284843e5b
larkz <liu.larkin@gmail.com>
2021-12-22 15:33:31 +0100

    use q learning

--- DQN.py
+++ DQN.py
@@ -87,11 +87,11 @@ class DQN(Agent):
         # potentially use difference between current_value and next_value
         self.actor_optimizer.zero_grad()
         if self.critic_loss == "huber":
-            # loss = th.nn.functional.smooth_l1_loss(current_q, target_q)
-            loss = th.nn.functional.smooth_l1_loss(current_value + nash_epsilon, next_value)
+            loss = th.nn.functional.smooth_l1_loss(current_q, target_q)
+            # loss = th.nn.functional.smooth_l1_loss(current_value + nash_epsilon, next_value)
         else:
-            # loss = th.nn.MSELoss()(current_q, target_q)
-            loss = th.nn.MSELoss()(current_value + nash_epsilon, next_value)
+            loss = th.nn.MSELoss()(current_q, target_q)
+            # loss = th.nn.MSELoss()(current_value + nash_epsilon, next_value)
         loss.backward()
         if self.max_grad_norm is not None:
             nn.utils.clip_grad_norm(self.actor.parameters(), self.max_grad_norm)
