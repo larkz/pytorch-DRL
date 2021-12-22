e46d10e6060502e3f62b56f929742c7ff6f92765
larkz <liu.larkin@gmail.com>
2021-12-22 11:40:03 +0100

    loss on q

--- DQN.py
+++ DQN.py
@@ -81,17 +81,17 @@ class DQN(Agent):
         # compute target q by: r + gamma * max_a { V(s_{t+1}) }
         target_q = self.reward_scale * rewards_var + self.reward_gamma * next_value * (1. - dones_var)
 
-        epsilon = 0.0
+        nash_epsilon = 0.0
 
         # update value network
         # potentially use difference between current_value and next_value
         self.actor_optimizer.zero_grad()
         if self.critic_loss == "huber":
             loss = th.nn.functional.smooth_l1_loss(current_q, target_q)
-            # loss = th.nn.functional.smooth_l1_loss(current_value + epsilon, next_value)
+            # loss = th.nn.functional.smooth_l1_loss(current_value + nash_epsilon, next_value)
         else:
-            loss = th.nn.MSELoss()(current_q + epsilon, target_q)
-            # loss = th.nn.MSELoss()(current_value + epsilon, next_value)
+            loss = th.nn.MSELoss()(current_q, target_q)
+            # loss = th.nn.MSELoss()(current_value + nash_epsilon, next_value)
         loss.backward()
         if self.max_grad_norm is not None:
             nn.utils.clip_grad_norm(self.actor.parameters(), self.max_grad_norm)
--- run_dqn.py
+++ run_dqn.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-MAX_EPISODES = 5000
+MAX_EPISODES = 2000
 EPISODES_BEFORE_TRAIN = 0
 EVAL_EPISODES = 10
 EVAL_INTERVAL = 100
